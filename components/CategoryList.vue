2c2cd8265c158b50275634b878f2145d469e191b
Jared Long <jlong4223@gmail.com>
2021-03-23 14:17:30 -0500

    adds category list and view components

---
+++ components/CategoryList.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li v-for="category in categories" :key="category.slug">
+      <n-link
+        :to="{
+          name: 'categories-slug',
+          params: { slug: category.slug },
+        }"
+      >
+        <category-view :category="category"></category-view>
+      </n-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import CategoryView from "./CategoryView.vue";
+export default {
+  components: { CategoryView },
+  props: ["categories"],
+};
+
+/* Here I am mapping over the categories and creating a new link to show the category name - each category is passed as props to the category view component */
+</script>
