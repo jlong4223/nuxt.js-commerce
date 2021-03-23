a25735ec2807920c09a5aaf4707dae4ad71afd86
Jared Long <jlong4223@gmail.com>
2021-03-23 13:58:35 -0500

    creates view and list component and passes products props

---
+++ components/ProductList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="product in products" :key="product.permalink">
+      <n-link
+        :to="{
+          name: 'products-permalink',
+          params: { permalink: product.permalink },
+        }"
+      >
+        <product-view :product="product"></product-view>
+      </n-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import ProductView from "./ProductView.vue";
+export default {
+  components: { ProductView },
+  props: ["products"],
+};
+</script>
